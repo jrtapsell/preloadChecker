a1d15152af1150bef42b1473e34b712f4d11f750
James Tapsell <email@jrtapsell.co.uk>
2017-11-24 11:24:56 +0000

    Restyled the UI

--- preloadChecker.py
+++ preloadChecker.py
@@ -32,6 +32,12 @@ def hello_world(url):
 @app.route("/")
 def index():
     return send_file("static/index.html")
+
+@app.route("/<path>")
+def resultsPage(path):
+    return send_file("static/index.html")
+
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
--- static/index.js
+++ static/index.js
@@ -1,10 +1,26 @@
 var data = $("#siteURL");
 var body = $("#data");
 var statusArea = $("#status");
+var progressBar = $("#p2");
 
 $("#load").click(function() {
-    body.empty();
     var url = data.val();
+    window.location = "/" + url
+});
+
+$(function () {
+   var url = window.location.pathname.substr(1);
+   data.val(url);
+   if (url !== "") {
+       setLoading();
+       loadURL(url);
+   } else {
+       setDone()
+   }
+});
+
+function loadURL(url) {
+    body.empty();
     document.title = "HSTS Status of: " + url;
     $.get("/api/" + url, undefined, function (data) {
            console.log(data);
@@ -13,5 +29,15 @@ $("#load").click(function() {
                var template = Handlebars.compile(templateUncomp);
                 body.html(template(data))
             });
+            setDone();
     }, "json");
-})
+}
+
+function setDone() {
+    progressBar.removeClass("mdl-progress__indeterminate");
+    progressBar[0].MaterialProgress.setProgress(100);
+}
+
+function setLoading() {
+    progressBar.addClass("mdl-progress__indeterminate");
+}
